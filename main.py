56b1a7bf62cd2b5b7cf116a5dfae92019f296e59
Zheng Yan Wong <zheng-yan.wong@keysight.com>
2022-11-07 11:37:22 +0800

    adding main.py, requirements.txt

---
+++ main.py
@@ -0,0 +1,15 @@
+import fastapi
+import uvicorn
+
+print("Hello fastapi")
+api = fastapi.FastAPI()
+
+@api.get("/")
+def index():
+    return {"msg": "This is the landing page"}
+
+@api.get("/api/endpoint")
+def endpoint():
+    return {"msg": "Hello FastAPI"}
+
+uvicorn.run(api,port=9000)
